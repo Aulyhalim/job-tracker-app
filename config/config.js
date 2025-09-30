9931b34bbdf55e450e2d8f57edf09dc9a9513f96
Auly Halim <aulyhalim08@gmail.com>
2025-10-01 01:14:31 +0700

    fix: final deployment configuration

--- config/config.js
+++ config/config.js
@@ -7,11 +7,13 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql"
+    use_env_variable: "DATABASE_URL",
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
