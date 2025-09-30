99e6dca77559c5ea166cec9d0f120f2e7d092fe7
Auly Halim <aulyhalim08@gmail.com>
2025-10-01 01:05:36 +0700

    refactor: use explicit DATABASE_URL variable

--- config/config.js
+++ config/config.js
@@ -16,12 +16,13 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql"
-    // Bagian dialectOptions dihapus dari sini
+    use_env_variable: "DATABASE_URL",
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
