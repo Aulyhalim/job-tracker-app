8ad42a495ce0613d22172335294cf06c1bc5fdeb
Auly Halim <aulyhalim08@gmail.com>
2025-10-01 15:09:14 +0700

    trigger: redeploy with pre-deploy step

--- models/index.js
+++ models/index.js
@@ -2,19 +2,11 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// PENTING: Impor koneksi sequelize yang sudah ada dari migrator kita
+const { sequelize } = require('../db/runMigrations'); 
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const basename = path.basename(__filename);
 
 fs
   .readdirSync(__dirname)
@@ -27,7 +19,8 @@ fs
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    // Gunakan instance sequelize yang diimpor untuk memuat model
+    const model = require(path.join(__dirname, file))(sequelize, require('sequelize').DataTypes);
     db[model.name] = model;
   });
 
@@ -38,6 +31,6 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.Sequelize = require('sequelize');
 
 module.exports = db;
